0945f7107ec3e0e7f98f990352560fd45fc36ce9
WesleyBurgt <wesleyburgt@gmail.com>
2024-11-21 12:11:09 +0100

    Added pokemonItemComponent seperators

--- src/components/pokemonItemComponent.tsx
+++ src/components/pokemonItemComponent.tsx
@@ -5,13 +5,13 @@ import { Item, Pokemon } from '@/app//types'
 import { Input } from '@/components/ui/input'
 
 interface PokemonItemTabProps {
-    pokemon: Pokemon
-    updatePokemon: (pokemon: Pokemon) => void
-    setView: (view: 'list' | 'statTab' | 'itemTab' | 'abilityTab' | 'moveTab' | 'team' | 'teamList') => void
-    items: Item[]
+    pokemon: Pokemon;
+    updatePokemon: (pokemon: Pokemon) => void;
+    setView: (view: 'list' | 'statTab' | 'itemTab' | 'abilityTab' | 'moveTab' | 'team' | 'teamList') => void;
+    items: Item[];
 }
 
-export default function PokemonItemComponent({ pokemon, updatePokemon, setView, items }: PokemonItemTabProps) {
+export default function PokemonItemComponent({pokemon, updatePokemon, setView, items}: PokemonItemTabProps) {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredItemList, setFilteredItemList] = useState<Item[]>(items);
 
@@ -23,6 +23,19 @@ export default function PokemonItemComponent({ pokemon, updatePokemon, setView, 
         setFilteredItemList(filteredList);
     }, [searchTerm, items]);
 
+    const pokemonSpecificItems = filteredItemList.filter(item =>
+        item.description?.toLowerCase().includes(pokemon.name.toLowerCase())
+    );
+    const heldItems = filteredItemList.filter(
+        item => item.description?.toLowerCase().includes('held: ') && 
+                !item.description?.toLowerCase().includes(pokemon.name.toLowerCase())
+    );
+    const otherItems = filteredItemList.filter(
+        item => 
+            !item.description?.toLowerCase().includes('held: ') &&
+            !item.description?.toLowerCase().includes(pokemon.name.toLowerCase())
+    );
+
     return (
         <div>
             <Card>
@@ -33,7 +46,7 @@ export default function PokemonItemComponent({ pokemon, updatePokemon, setView, 
                                 type="text"
                                 placeholder="Search Item"
                                 value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
+                                onChange={e => setSearchTerm(e.target.value)}
                                 className="pl-10"
                             />
                             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-400" />
@@ -46,45 +59,126 @@ export default function PokemonItemComponent({ pokemon, updatePokemon, setView, 
                                 </tr>
                             </thead>
                             <tbody>
-                                {searchTerm.length == 0 && (
+                                {searchTerm.length === 0 && (
                                     <tr
                                         key={'item-0'}
                                         className={`cursor-pointer hover:bg-gray-100`}
                                         onClick={() => {
-                                            updatePokemon({ ...pokemon, item: null })
-                                            setView("abilityTab")
+                                            updatePokemon({ ...pokemon, item: null });
+                                            setView('abilityTab');
                                         }}
                                     >
-                                        <td className="p-2 flex items-center">
-                                            no item
-                                        </td>
+                                        <td className="p-2 flex items-center">No item</td>
                                         <td className="p-2"></td>
                                     </tr>
                                 )}
-                                {filteredItemList.map((item, index) => (
-                                    <tr
-                                        key={`item-${item.id}`}
-                                        className={`cursor-pointer hover:bg-gray-100 ${(index + 1) % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}
-                                        onClick={() => {
-                                            updatePokemon({ ...pokemon, item: item })
-                                            setView("abilityTab")
-                                        }}
-                                    >
-                                        <td className="p-2 flex items-center">
-                                            {item.image && (
-                                                <img src={item.image} alt={item.name} className="w-10 h-10 mr-2" loading="lazy" />
-                                            )} {item.name}
-                                        </td>
-                                        <td className="p-2">
-                                            {item.description}
-                                        </td>
-                                    </tr>
-                                ))}
+
+                                {/* Pokémon Specific Items */}
+                                {pokemonSpecificItems.length > 0 && (
+                                    <>
+                                        <tr className="bg-zinc-200">
+                                            <td colSpan={2} className="p-2 font-bold">
+                                                Pokémon Specific Items
+                                            </td>
+                                        </tr>
+                                        {pokemonSpecificItems.map((item, index) => (
+                                            <tr
+                                                key={`item-${item.id}`}
+                                                className={`cursor-pointer hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}
+                                                onClick={() => {
+                                                    updatePokemon({ ...pokemon, item });
+                                                    setView('abilityTab');
+                                                }}
+                                            >
+                                                <td className="p-2 flex items-center">
+                                                    {item.image && (
+                                                        <img
+                                                            src={item.image}
+                                                            alt={item.name}
+                                                            className="w-10 h-10 mr-2"
+                                                            loading="lazy"
+                                                        />
+                                                    )}
+                                                    {item.name}
+                                                </td>
+                                                <td className="p-2">{item.description}</td>
+                                            </tr>
+                                        ))}
+                                    </>
+                                )}
+
+                                {/* Held Items */}
+                                {heldItems.length > 0 && (
+                                    <>
+                                        <tr className="bg-zinc-200">
+                                            <td colSpan={2} className="p-2 font-bold">
+                                                Held Items
+                                            </td>
+                                        </tr>
+                                        {heldItems.map((item, index) => (
+                                            <tr
+                                                key={`item-${item.id}`}
+                                                className={`cursor-pointer hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}
+                                                onClick={() => {
+                                                    updatePokemon({ ...pokemon, item });
+                                                    setView('abilityTab');
+                                                }}
+                                            >
+                                                <td className="p-2 flex items-center">
+                                                    {item.image && (
+                                                        <img
+                                                            src={item.image}
+                                                            alt={item.name}
+                                                            className="w-10 h-10 mr-2"
+                                                            loading="lazy"
+                                                        />
+                                                    )}
+                                                    {item.name}
+                                                </td>
+                                                <td className="p-2">{item.description}</td>
+                                            </tr>
+                                        ))}
+                                    </>
+                                )}
+
+                                {/* Other Items */}
+                                {otherItems.length > 0 && (
+                                    <>
+                                        <tr className="bg-zinc-200">
+                                            <td colSpan={2} className="p-2 font-bold">
+                                                Other Items
+                                            </td>
+                                        </tr>
+                                        {otherItems.map((item, index) => (
+                                            <tr
+                                                key={`item-${item.id}`}
+                                                className={`cursor-pointer hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}
+                                                onClick={() => {
+                                                    updatePokemon({ ...pokemon, item });
+                                                    setView('abilityTab');
+                                                }}
+                                            >
+                                                <td className="p-2 flex items-center">
+                                                    {item.image && (
+                                                        <img
+                                                            src={item.image}
+                                                            alt={item.name}
+                                                            className="w-10 h-10 mr-2"
+                                                            loading="lazy"
+                                                        />
+                                                    )}
+                                                    {item.name}
+                                                </td>
+                                                <td className="p-2">{item.description}</td>
+                                            </tr>
+                                        ))}
+                                    </>
+                                )}
                             </tbody>
                         </table>
                     </div>
                 </CardContent>
             </Card>
         </div>
-    )
+    );
 }
