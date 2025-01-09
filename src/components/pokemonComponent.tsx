6dca54abd210a8f4217be11574454ddaf9351c57
WesleyBurgt <wesleyburgt@gmail.com>
2025-01-09 10:48:14 +0100

    moved calculateDerivedStats to own file with export to prevent double function existing also added 1 hp baseStat exception

--- src/components/pokemonComponent.tsx
+++ src/components/pokemonComponent.tsx
@@ -13,6 +13,7 @@ import { Card } from "@/components/ui/card"
 import { Copy, Download, ArrowUpDown, Trash2 } from 'lucide-react'
 import { BasePokemon, Pokemon } from '@/app//types'
 import PokemonTypingComponent from './pokemonTyping'
+import { calculateDerivedStats, statAbbreviations } from '@/app/StatCalculator'
 
 interface ComponentProps {
     pokemon: Pokemon
@@ -24,57 +25,6 @@ interface ComponentProps {
     view: 'list' | 'statTab' | 'itemTab' | 'abilityTab' | 'moveTab' | 'team' | 'teamList'
     setView: (view: 'list' | 'statTab' | 'itemTab' | 'abilityTab' | 'moveTab' | 'team' | 'teamList') => void
     genders: string[]
-}
-
-const statAbbreviations: { [key in keyof BasePokemon['baseStats']]: string } = {
-    hp: 'HP',
-    attack: 'Atk',
-    defense: 'Def',
-    specialAttack: 'SpA',
-    specialDefense: 'SpD',
-    speed: 'Spe',
-};
-
-export function calculateDerivedStats(pokemon: Pokemon): BasePokemon['baseStats'] {
-    if (!pokemon.basePokemon) {
-        console.warn(`BasePokemon data is missing for Pokémon with personalId: ${pokemon.personalId}`);
-        return {
-            hp: 0,
-            attack: 0,
-            defense: 0,
-            specialAttack: 0,
-            specialDefense: 0,
-            speed: 0,
-        };
-    }
-
-    const level = pokemon.level;
-
-    const calculateStat = (
-        baseStat: number,
-        ev: number,
-        iv: number,
-        natureMultiplier: number
-    ): number => {
-        return Math.floor(((((2 * baseStat + iv + Math.floor(ev / 4)) * level) / 100) + 5) * natureMultiplier);
-    };
-
-    const getNatureMultiplier = (stat: keyof BasePokemon['baseStats']): number => {
-        if (pokemon.nature.up == stat) return 1.1;
-        if (pokemon.nature.down == stat) return 0.9;
-        return 1;
-    };
-
-    const derivedStats: BasePokemon['baseStats'] = {
-        hp: Math.floor(((2 * pokemon.basePokemon.baseStats.hp + pokemon.iVs.hp + Math.floor(pokemon.eVs.hp / 4)) * level) / 100) + level + 10,
-        attack: calculateStat(pokemon.basePokemon.baseStats.attack, pokemon.eVs.attack, pokemon.iVs.attack, getNatureMultiplier('attack')),
-        defense: calculateStat(pokemon.basePokemon.baseStats.defense, pokemon.eVs.defense, pokemon.iVs.defense, getNatureMultiplier('defense')),
-        specialAttack: calculateStat(pokemon.basePokemon.baseStats.specialAttack, pokemon.eVs.specialAttack, pokemon.iVs.specialAttack, getNatureMultiplier('specialAttack')),
-        specialDefense: calculateStat(pokemon.basePokemon.baseStats.specialDefense, pokemon.eVs.specialDefense, pokemon.iVs.specialDefense, getNatureMultiplier('specialDefense')),
-        speed: calculateStat(pokemon.basePokemon.baseStats.speed, pokemon.eVs.speed, pokemon.iVs.speed, getNatureMultiplier('speed'))
-    };
-
-    return derivedStats;
 }
 
 export default function pokemonComponent({ pokemon, updatePokemon, deletePokemonFromTeam, setSelectedPokemon, selectedMoveSlot, setSelectedMoveSlot, view, setView, genders }: ComponentProps) {
--- src/components/pokemonStatComponent.tsx
+++ src/components/pokemonStatComponent.tsx
@@ -9,63 +9,13 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { Card, CardContent } from "@/components/ui/card"
-import { Nature, BasePokemon, Pokemon } from '@/app//types'
-
-const statAbbreviations: { [key: string]: string } = {
-    hp: 'HP',
-    attack: 'Atk',
-    defense: 'Def',
-    specialAttack: 'SpA',
-    specialDefense: 'SpD',
-    speed: 'Spe',
-};
+import { Nature, Pokemon } from '@/app//types'
+import { calculateDerivedStats, statAbbreviations } from '@/app/StatCalculator'
 
 interface PokemonStatTabProps {
     pokemon: Pokemon
     updatePokemon: (pokemon: Pokemon) => void
     natures: Nature[]
-}
-
-function calculateDerivedStats(pokemon: Pokemon): BasePokemon['baseStats'] {
-    if (!pokemon.basePokemon) {
-        console.warn(`BasePokemon data is missing for Pokémon with personalId: ${pokemon.personalId}`);
-        return {
-            hp: 0,
-            attack: 0,
-            defense: 0,
-            specialAttack: 0,
-            specialDefense: 0,
-            speed: 0,
-        };
-    }
-    
-    const level = pokemon.level;
-
-    const calculateStat = (
-        baseStat: number,
-        ev: number,
-        iv: number,
-        natureMultiplier: number
-    ): number => {
-        return Math.floor(((((2 * baseStat + iv + Math.floor(ev / 4)) * level) / 100) + 5) * natureMultiplier);
-    };
-
-    const getNatureMultiplier = (stat: keyof BasePokemon['baseStats']): number => {
-        if (pokemon.nature.up == stat) return 1.1;
-        if (pokemon.nature.down == stat) return 0.9;
-        return 1;
-    };
-
-    const derivedStats: BasePokemon['baseStats'] = {
-        hp: Math.floor(((2 * pokemon.basePokemon.baseStats.hp + pokemon.iVs.hp + Math.floor(pokemon.eVs.hp / 4)) * level) / 100) + level + 10,
-        attack: calculateStat(pokemon.basePokemon.baseStats.attack, pokemon.eVs.attack, pokemon.iVs.attack, getNatureMultiplier('attack')),
-        defense: calculateStat(pokemon.basePokemon.baseStats.defense, pokemon.eVs.defense, pokemon.iVs.defense, getNatureMultiplier('defense')),
-        specialAttack: calculateStat(pokemon.basePokemon.baseStats.specialAttack, pokemon.eVs.specialAttack, pokemon.iVs.specialAttack, getNatureMultiplier('specialAttack')),
-        specialDefense: calculateStat(pokemon.basePokemon.baseStats.specialDefense, pokemon.eVs.specialDefense, pokemon.iVs.specialDefense, getNatureMultiplier('specialDefense')),
-        speed: calculateStat(pokemon.basePokemon.baseStats.speed, pokemon.eVs.speed, pokemon.iVs.speed, getNatureMultiplier('speed'))
-    };
-
-    return derivedStats;
 }
 
 function calculateTotalEVs(evs: Pokemon['eVs']): number {
